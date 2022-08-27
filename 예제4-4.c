197de5d52ed7471e4ff4fa6f7793ba302ca8c4e5
heebum99 <heebum9955@naver.com>
2022-08-27 14:50:56 +0900

    전위, 후위 증감연산자

---
+++ 예제4-4.c
@@ -0,0 +1,15 @@
+#include "khb.h"
+
+int main()
+{
+	int a = 5, b = 5;
+	int pre, post;
+
+	pre = (++a) * 3; //전위 증감 연산자
+	post = (b++) * 3; //후위 증감 연산자
+
+	printf("초깃값 a = %d, b = %d\n", a, b);
+	printf("전위형: (++a) * 3 = %d, 후위형 : (b++) * 3 = %d\n", pre, post);
+
+	return 0;
+}
