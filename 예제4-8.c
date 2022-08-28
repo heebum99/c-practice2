6a56d6be843fd69b83304d192392df35231dd413
heebum99 <heebum9955@naver.com>
2022-08-28 20:33:46 +0900

    형 변환 연산자

---
+++ 예제4-8.c
@@ -0,0 +1,16 @@
+#include "khb.h"
+
+int main() 
+{
+	int a = 10, b = 3;
+	double res;
+
+	res = (double)a / (double)b;
+	printf("a = %d, b = %d\n", a, b);
+	printf("a / b의 결과 : %.1lf\n", res);
+
+	a = (int)res;
+	printf("(int) %.1lf의 결과 : %d\n", res, a);
+
+	return 0;
+}
