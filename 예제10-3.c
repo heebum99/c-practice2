1fe4962e07f6d149ea0e6b753255771c32ae8ac6
heebum99 <heebum9955@naver.com>
2022-09-07 17:59:47 +0900

    포인터를 이용한 배열 값 출력

---
+++ 예제10-3.c
@@ -0,0 +1,17 @@
+#include "khb.h"
+
+int main()
+{
+	int ary[3] = { 10,20,30 };
+	int* pa = ary;
+	int i;
+
+	printf("배열의 값 : ");
+	for (i = 0; i < 3; i++)
+	{
+		printf("%d ", *pa); //포인터 pa가 가리키는 배열 요소 출력
+		pa++; //다음 배열 요소를 가리키도록 pa 값 증가
+	}
+
+	return 0;
+}
