4ce382aa1711d6f7eeffeebb7b0eb9765dfced1b
heebum99 <heebum9955@naver.com>
2022-09-07 17:07:12 +0900

    배열명으로 배열요소 사용

---
+++ 예제10-1.c
@@ -0,0 +1,21 @@
+#include "khb.h"
+
+int main()
+{
+	int ary[3];
+	int i;
+
+	*(ary + 0) = 10; //포인터식 표현 == ary[0] = 10 (배열식 표현)
+	*(ary + 1) = *(ary + 0) + 10; //ary[1] = ary[0] + 10
+
+	printf("세 번째 배열 요소에 키보드 입력 : ");
+	scanf("%d", ary + 2); //&ary[2]
+
+	for (i = 0; i < 3; i++)
+	{
+		printf("%5d", *(ary + i)); //ary[i]
+	}
+
+
+	return 0;
+}
